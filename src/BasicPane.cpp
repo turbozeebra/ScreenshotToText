6cc656268c911f71683b18ac73c6df89857ad4fc
Niko <niko.nasti@protonmail.com>
2025-06-01 11:41:58 +0300

    remove wrong comments

--- src/BasicPane.cpp
+++ src/BasicPane.cpp
@@ -13,13 +13,7 @@ wxPanel(parent), squareCoordinates(squareCoordinates)
  
 }
 
-/*
- * Called by the system of by wxWidgets when the panel needs
- * to be redrawn. You can also trigger this call by
- * calling Refresh()/Update().
- */
-
- void BasicPane::update_screen_size(wxSize size){
+void BasicPane::update_screen_size(wxSize size){
     screen_size = size;
  }
 
@@ -60,30 +54,11 @@ void BasicPane::MousePressed(wxMouseEvent& event)
     mouse_released = false;
 }
 
-
-/*
- * Alternatively, you can use a clientDC to paint on the panel
- * at any time. Using this generally does not free you from
- * catching paint events, since it is possible that e.g. the window
- * manager throws away your drawing when the window comes to the
- * background, and expects you will redraw it when the window comes
- * back (by sending a paint event).
- *
- * In most cases, this will not be needed at all; simply handling
- * paint events and calling Refresh() when a refresh is needed
- * will do the job.
- */
 void BasicPane::paintNow()
 {
     wxClientDC dc(this);
     render(dc);
 }
-
-/*
- * Here we do the actual rendering. I put it in a separate
- * method so that it can work no matter what type of DC
- * (e.g. wxPaintDC or wxClientDC) is used.
- */
 
 void BasicPane::second_render(wxDC& dc, wxImage img){
     if (rec_to_draw) 
